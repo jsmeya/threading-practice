a7edaa22b9e656f07a5a488369cb10b9b66e645c
Justin Smeya <justinsmeya@gmail.com>
2026-08-27 10:33:47 -0500

    replaced args with thread object

--- threading/main.py
+++ threading/main.py
@@ -8,13 +8,13 @@ from time import sleep
 thread_count = 5 # Desired number of threads for testing
 threads = [] # A list to store each thread
 
-def print_thread(i: int):
-    print(f"This function is running on thread: {i}")
+def print_thread():
+    current = threading.current_thread()
+    print(f"This function is running on thread: {current}")
 
 for i in range(1, thread_count + 1):
     # target represents the desired function to run on the specified thread
-    # args are the arguments passed to the target function
-    thread = threading.Thread(target=print_thread, args=(i,)) # creates a new thread
+    thread = threading.Thread(target=print_thread) # creates a new thread
     thread.start() # runs the thread
     threads.append(thread) # adds the thread to the list
 
